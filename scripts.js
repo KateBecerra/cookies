8f1e6f3a9a05649fc2bd382b57b5efc07b44ca9e
KateBecerra <kate.becerra@students.dominican.edu>
2021-05-17 05:04:09 -0700

    another commit

--- scripts.js
+++ scripts.js
@@ -77,9 +77,11 @@ let refreshPowerClick = function() {
           Grandmas
 
 ********************************/
+
+
 //set default values
 let grandmaPower = 50;
-let grandmaPriceAmount = 500;
+let grandmaPriceAmount = 300;
 let grandmaLevelNumber = 0;
 let grandmaAuto = false;
 
@@ -93,35 +95,87 @@ let grandmaMultiple = document.getElementById('grandma-multiple');
 buyGrandma.addEventListener("click", function() {
   //make sure we have enough cookies and subtract our cookies from the price
   if (cookieCount >= grandmaPriceAmount) {
-     // Subtract cookies from the price of the item.
-     cookieCount +=  - grandmaPriceAmount;
-     refreshCookieCount()
+   // Subtract cookies from the price of the item.
+   cookieCount +=  - grandmaPriceAmount;
+   refreshCookieCount()
     //upgrade power level
-      grandmaLevelNumber += 1;
+    grandmaLevelNumber += 1;
     //update price
-      grandmaPriceAmount = Math.floor(grandmaPriceAmount * 1.33);
+    grandmaPriceAmount = Math.floor(grandmaPriceAmount * 1.33);
     //update grandma power
-      grandmaPower += 10;
+    grandmaPower += 10;
     //turn autoGrandma on!
-      autoGrandma = true
-      autoGrandmaStart();
+    autoGrandma = true
+    autoGrandmaStart();
     //refresh shop item
-      refreshGrandma();
-    }
+    refreshGrandma();
+  }
+})
 
     let refreshGrandma = function() {
       grandmaLevel.innerHTML = grandmaLevelNumber
       grandmaPrice.innerHTML = grandmaPriceAmount;
       grandmaMultiple.innerHTML = grandmaPower;
+      grandmaMultiple.innerHTML = grandmaPower - 10;
     }
-    window.setInterval(function(){
-      let autoGrandmaStart = function() {
-      let grandmaInt = window.setInterval(function(){
-        cookieCount += grandmaPower;
+
+    let autoGrandmaStart = function() {
+    let grandmaInt = window.setInterval(function(){
+      cookieCount += grandmaPower;
+      refreshCookieCount();
+    }, 1000);
+  }
+
+/********************************
+
+          Facilities
+
+********************************/
+  //set default variables
+  let facilityAuto = false;
+  let facilityPower = 2000;
+  let facilityPriceAmount = 100000;
+  let facilityLevelNumber = 0;
+
+  //declare DOM variables
+  let buyFacility = document.getElementById('buy-facility');
+  let facilityPrice = document.getElementById('facility-price');
+  let facilityLevel = document.getElementById('facility-level');
+  let facilityMultiple = document.getElementById('facility-multiple');
+
+  //buy a facility
+  buyFacility.addEventListener("click", function() {
+      //set autoLoop to false
+      facilityAuto = false;
+      //make sure we have enough cookies
+      if (cookieCount >= facilityPriceAmount) {
+        cookieCount -= facilityPriceAmount;
+        refreshCookieCount()
+       //upgrade power level
+       facilityLevelNumber += 1;
+       //update price
+       facilityPriceAmount = Math.floor(facilityPriceAmount * 1.33);
+       //update facility power
+       facilityPower += 600;
+       //turn autoFacility on!
+       facilityAuto = true
+       autoFacilityStart();
+       //refresh shop item
+       refreshFacility();
+     }
+    })
+
+
+  //game loop
+    let autoFacilityStart = function() {
+      let facilityInt = window.setInterval(function(){
+        cookieCount += facilityPower;
         refreshCookieCount();
       }, 1000);
+    }
+  //refresh shop
+    let refreshFacility = function() {
+    facilityLevel.innerHTML = facilityLevelNumber
+    facilityPrice.innerHTML = facilityPriceAmount;
+    facilityMultiple.innerHTML = facilityPower - 600;
   }
-    }, numberOfMilliSeconds)
-
-}
-)
